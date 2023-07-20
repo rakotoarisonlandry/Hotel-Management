35ad249c0b82aa4b43c891494b06eeddf0d899a0
rakotorisonlandry <rakotorisonlandry@gmail.com>
2023-07-20 20:47:17 +0200

    Design Chambre page

--- src/main/java/com/landry/hotel/Controllers/ContentAllController.java
+++ src/main/java/com/landry/hotel/Controllers/ContentAllController.java
@@ -1,8 +1,6 @@
     package com.landry.hotel.Controllers;
 
     import com.landry.hotel.DB.DBConnection;
-    import javafx.collections.FXCollections;
-    import javafx.collections.ObservableList;
     import javafx.fxml.FXML;
     import javafx.fxml.FXMLLoader;
     import javafx.fxml.Initializable;
@@ -39,10 +37,7 @@
             }catch (IOException ex){
                 Logger.getLogger(ContentAllController.class.getName()).log(Level.SEVERE ,null,ex);
             }
-
         }
-
-
 
         public  void Chambre (javafx.event.ActionEvent actionEvent) throws  IOException{
             Parent fxml =FXMLLoader.load(getClass().getResource("/FXML/Chambre.fxml"));
@@ -67,29 +62,6 @@
             ContentArea.getChildren().removeAll();
             ContentArea.getChildren().setAll(fxml);
         }
-    //    public void Soldeactuel(Label soldeIdLabel) throws SQLException {
-    //        Connection conn = null;
-    //        try {
-    //            DBConnection dbConnection = new DBConnection();
-    //            conn = dbConnection.getConnection("hotel", "root", "");
-    //
-    //            ResultSet rs = conn.createStatement().executeQuery("SELECT `SoldeActuel` FROM solde WHERE idSolde = 1");
-    //
-    //            if (rs.next()) {
-    //                double soldeActuel = rs.getDouble(1); // Récupérer le résultat en tant que double
-    //                System.out.println(soldeActuel);
-    //                soldeIdLabel.setText(String.valueOf(soldeActuel) + " Ariary"); // Convertir le double en chaîne de caractères pour l'affichage
-    //                System.out.println(soldeIdLabel);
-    //            }
-    //        } catch (Exception e) {
-    //            e.printStackTrace();
-    //        } finally {
-    //            // Fermer la connexion
-    //            if (conn != null) {
-    //                conn.close();
-    //            }
-    //        }
-    //    }
 
         public double Soldeactuel() throws SQLException {
             Connection conn = null;
@@ -111,30 +83,6 @@
             }
             return 0.0; // Ajout d'un retour par défaut au cas où aucun solde n'est trouvé
         }
-
-//        public void Soldeactuel(Label soldeIdLabel) throws SQLException {
-//            Connection conn = null;
-//            try {
-//                DBConnection dbConnection = new DBConnection();
-//                conn = dbConnection.getConnection("hotel", "root", "");
-//
-//                ResultSet rs = conn.createStatement().executeQuery("SELECT `SoldeActuel` FROM solde WHERE idSolde = 1");
-//
-//                if (rs.next()) {
-//                    double soldeActuel = rs.getDouble(1); // Récupérer le résultat en tant que double
-//                    System.out.println(soldeActuel);
-//                    soldeIdLabel.textProperty().set(String.valueOf(soldeActuel) + " Ariary"); // Modifier la propriété text de soldeIdLabel
-//                    System.out.println(soldeIdLabel);
-//                }
-//            } catch (Exception e) {
-//                e.printStackTrace();
-//            } finally {
-//                // Fermer la connexion
-//                if (conn != null) {
-//                    conn.close();
-//                }
-//            }
-//        }
 
     }
 
